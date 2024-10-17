7501a1c641ffe48af4904a50b661f9a7e0f991f7
VIRTUOSGAMESdKrasnikov <dkrasnikov@virtuosgames.com>
2024-10-17 12:41:20 +0300

    added mesh combiner

---
+++ DiplomaProjectUnity/Assets/Scripts/ToolsWindows/Editor/MeshCombiner.cs
@@ -0,0 +1,65 @@
+﻿using Sirenix.OdinInspector;
+using Sirenix.OdinInspector.Editor;
+using UnityEditor;
+using UnityEngine;
+
+namespace DiplomaProject.ToolsWindows.Editor
+{
+    public class MeshCombiner : OdinEditorWindow
+    {
+        [SerializeField] private MeshFilter[] _meshes;
+        [SerializeField] private MeshFilter _targetMesh;
+
+        [MenuItem("Tools/Combine Meshes")]
+        private static void OpenWindow()
+        {
+            GetWindow<MeshCombiner>().Show();
+        }
+        
+        [Button("Combine Meshes")]
+        public void CombineMeshes()
+        {
+            var combine = new CombineInstance[_meshes.Length];
+
+            int i = 0;
+            foreach (var mesh in _meshes)
+            {
+                combine[i].mesh = mesh.sharedMesh;
+                combine[i].transform = mesh.transform.localToWorldMatrix;
+
+                i++;
+            }
+            
+            var newMesh = new Mesh();
+            newMesh.CombineMeshes(combine);
+            
+            _targetMesh.mesh = newMesh;
+            _targetMesh.GetComponent<MeshRenderer>().sharedMaterials = _meshes[0].GetComponent<MeshRenderer>().sharedMaterials;
+
+            EditorUtility.SetDirty(_targetMesh);
+
+            foreach (var mesh in _meshes)   
+            {
+                mesh.gameObject.SetActive(false);
+                EditorUtility.SetDirty(mesh);
+            }
+            
+            SaveMesh(newMesh, true);
+        }
+
+        private void SaveMesh(Mesh mesh, bool optimize)
+        {
+            var path = EditorUtility.SaveFilePanel("Save combined mesh", "Assets", _targetMesh.name, "asset");
+            if (string.IsNullOrWhiteSpace(path)) return;
+            path = FileUtil.GetProjectRelativePath(path);
+
+            if (optimize)
+            {
+                MeshUtility.Optimize(mesh);
+            }
+            
+            AssetDatabase.CreateAsset(mesh, path);
+            AssetDatabase.SaveAssets();
+        }
+    }
+}
---
+++ DiplomaProjectUnity/Assets/Scripts/ToolsWindows/Editor/SortChildren.cs
@@ -0,0 +1,52 @@
+﻿using System.Collections.Generic;
+using System.Linq;
+using Sirenix.OdinInspector;
+using Sirenix.OdinInspector.Editor;
+using UnityEditor;
+using UnityEngine;
+
+namespace DiplomaProject.ToolsWindows.Editor
+{
+    public class SortChildren : OdinEditorWindow
+    {
+        [SerializeField] private Transform _parent;
+
+        [MenuItem("Tools/Sort Children")]
+        private static void OpenWindow()
+        {
+            GetWindow<SortChildren>().Show();
+        }
+
+        [Button("Sort Children")]
+        public void Sort()
+        {
+            // Create a list to store the child objects
+            List<Transform> children = new List<Transform>();
+
+            // Populate the list with the children of the parent
+            foreach (Transform child in _parent)
+            {
+                children.Add(child);
+            }
+
+            // Sort the list based on their names, extracting the numeric value and using it for sorting
+            children = children.OrderBy(child =>
+            {
+                // Match the prefix and extract the numeric part at the end of the name
+                string name = child.name;
+                string prefix = name.Substring(0, name.LastIndexOf('_'));
+                int number = int.Parse(name.Substring(name.LastIndexOf('_') + 1));
+                return (prefix, number);
+            }).ToList();
+
+            // Reorder the children in the hierarchy
+            for (int i = 0; i < children.Count; i++)
+            {
+                children[i].SetSiblingIndex(i);
+            }
+            
+            EditorUtility.SetDirty(_parent.gameObject);
+        }
+
+    }
+}
