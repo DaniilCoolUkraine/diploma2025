d2115d2eb1717511d451fa044f94d80325911cb1
VIRTUOSGAMESdKrasnikov <dkrasnikov@virtuosgames.com>
2024-10-29 20:22:54 +0200

    ecs playing

---
+++ DiplomaProjectUnity/Assets/Scripts/Test/Entities/RotateSpeedAuthoring.cs
@@ -0,0 +1,22 @@
+﻿using Unity.Entities;
+using UnityEngine;
+
+namespace DiplomaProject.Test.Entities
+{
+    public class RotateSpeedAuthoring : MonoBehaviour
+    {
+        private class Baker : Baker<RotateSpeedAuthoring>
+        {
+            public override void Bake(RotateSpeedAuthoring authoring)
+            {
+                var entity = GetEntity(TransformUsageFlags.Dynamic);
+                AddComponent(entity, new RotateSpeed { Speed = Random.Range(-5, 5) });
+            }
+        }
+    }
+    
+    public struct RotateSpeed : IComponentData
+    {
+        public float Speed;
+    }
+}
---
+++ DiplomaProjectUnity/Assets/Scripts/Test/Entities/RotateSystem.cs
@@ -0,0 +1,33 @@
+﻿using Unity.Burst;
+using Unity.Entities;
+using Unity.Transforms;
+
+namespace DiplomaProject.Test.Entities
+{
+    public partial struct RotateSystem : ISystem
+    {
+        public void OnCreate(ref SystemState state)
+        {
+            state.RequireForUpdate<RotateSpeed>();
+        }
+        
+        // [BurstCompile]
+        public void OnUpdate(ref SystemState state)
+        {
+            var entities = SystemAPI.QueryBuilder().WithAll<RotateSpeed>().WithAll<LocalTransform>().Build();
+            var job = new RotateJob{DeltaTime = SystemAPI.Time.DeltaTime};
+
+            job.ScheduleParallel(entities);
+        }
+    }
+    
+    [BurstCompile]
+    partial struct RotateJob : IJobEntity
+    {
+        public float DeltaTime;
+        public void Execute(ref LocalTransform transform, in RotateSpeed speed)
+        {
+            transform = transform.RotateY(speed.Speed * DeltaTime);
+        }
+    }
+}
