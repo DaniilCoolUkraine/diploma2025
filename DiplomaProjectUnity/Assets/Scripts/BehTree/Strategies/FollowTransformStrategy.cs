e0db9431befeeff844356938ba80a657aa98f3fd
VIRTUOSGAMESdKrasnikov <dkrasnikov@virtuosgames.com>
2025-01-26 18:45:03 +0200

    added run speed

--- DiplomaProjectUnity/Assets/Scripts/BehTree/Strategies/FollowTransformStrategy.cs
+++ DiplomaProjectUnity/Assets/Scripts/BehTree/Strategies/FollowTransformStrategy.cs
@@ -7,23 +7,27 @@ namespace DiplomaProject.BehTree.Strategies
     public class FollowTransformStrategy : IStrategy
     {
         private const int FRAMES_BETWEEN_UPDATE = 10;
+        private const int RUN_DISTANCE = 10;
         
         private NavMeshAgent _agent;
         private Transform _target;
         private Animator _animator;
-        private float _speed;
+
+        private float _walkSpeed;
+        private float _runSpeed;
 
         private int _framesWaiting = FRAMES_BETWEEN_UPDATE;
         
         private int _animIDSpeed = Animator.StringToHash("Speed");
         private int _animIDMotionSpeed = Animator.StringToHash("MotionSpeed");
         
-        public FollowTransformStrategy(NavMeshAgent agent, Transform target, Animator animator, float speed)
+        public FollowTransformStrategy(NavMeshAgent agent, Transform target, Animator animator, float walkSpeed, float runSpeed)
         {
             _agent = agent;
             _target = target;
             _animator = animator;
-            _speed = speed;
+            _walkSpeed = walkSpeed;
+            _runSpeed = runSpeed;
         }
         
         public Node.Status Process()
@@ -54,8 +58,8 @@ namespace DiplomaProject.BehTree.Strategies
                 _framesWaiting = 0;
                 
                 _agent.SetDestination(_target.position);
-                _agent.speed = _speed;
-                _animator.SetFloat(_animIDSpeed, _speed);
+                _agent.speed = Vector3.Distance(_target.position, _agent.transform.position) >= RUN_DISTANCE ? _runSpeed : _walkSpeed;
+                _animator.SetFloat(_animIDSpeed, _walkSpeed);
                 _animator.SetFloat(_animIDMotionSpeed, 1);
             }
 
--- DiplomaProjectUnity/Assets/Scripts/EnemyAI/Enemy.cs
+++ DiplomaProjectUnity/Assets/Scripts/EnemyAI/Enemy.cs
@@ -12,7 +12,8 @@ namespace DiplomaProject.EnemyAI
         [SerializeField] private NavMeshAgent _agent;
         [SerializeField] private Animator _animator;
         
-        [SerializeField] private float _speed;
+        [SerializeField] private float _walkSpeed;
+        [SerializeField] private float _runSpeed;
 
         [SerializeField] private Transform _target;
         [SerializeField, Required] private Transform _projectileSpawnPoint;
@@ -27,7 +28,7 @@ namespace DiplomaProject.EnemyAI
 
             var sequence = new LoopSequence("Follow target sequence")
                 .AddChild(new Leaf("Check target", new ConditionStrategy(() => _target.gameObject.activeSelf)))
-                .AddChild(new Leaf("Follow target", new FollowTransformStrategy(_agent, _target, _animator, _speed)))
+                .AddChild(new Leaf("Follow target", new FollowTransformStrategy(_agent, _target, _animator, _walkSpeed, _runSpeed)))
                 .AddChild(new Leaf("Attack", new AttackStrategy(_target, transform, _animator, _projectileSpawnPoint, _container)))
                 ;
 
